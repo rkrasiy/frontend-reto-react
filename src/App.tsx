86db7eb8f32e560f2a7e7dd42cbfe1003aaf0f7e
ruslan.krasiy <ruslan.krasiy@alten.es>
2023-12-18 08:57:24 +0100

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Grid } from '@mui/material'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Grid container>
+      <h1>Helo</h1>
+    </Grid>
+  )
+}
+
+export default App
